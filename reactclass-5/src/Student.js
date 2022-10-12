8eef2fdcf92fa1b59eb27c1440787655690fdc34
GauravBhardwaj <gaurav.bhardwaj@waveaxis.com>
2022-10-12 11:47:57 +0530

    Code Updated

--- reactclass-5/src/Student.js
+++ reactclass-5/src/Student.js
@@ -2,52 +2,90 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
 class Student extends Component {
-  constructor() {
-    super();
-    console.log("This Is Student Constructor");
-  }
+	constructor() {
+		super();
+		console.log("This Is Student Constructor");
+	}
 
-  ele = (<h2>JSX Hold In Variable.This Is Javascript Expression.</h2>);
+	ele = (<h2>JSX Hold In Variable.This Is Javascript Expression.</h2>);
 
-  // render() {
-  //   return (
-  //     <>
-  //       <h2>This Is Student Class Component...</h2>
-  //       {this.ele}
-  //     </>
-  //   );
-  // }
+	// render() {
+	//   return (
+	//     <>
+	//       <h2>This Is Student Class Component...</h2>
+	//       {this.ele}
+	//     </>
+	//   );
+	// }
 
-  // render() {
-  //   return (
-  //     <>
-  //       <h2>This Is Student Class Component...</h2>
-  //       <h2>This Is Student Class 2nd Component...</h2>
-  //     </>
-  //   );
-  // }
+	// render() {
+	//   return (
+	//     <>
+	//       <h2>This Is Student Class Component...</h2>
+	//       <h2>This Is Student Class 2nd Component...</h2>
+	//     </>
+	//   );
+	// }
 
-  mobNo = +917017651577;
+	mobNo = +917017651577;
 
-  city = "Bangalore";
+	city = "Bangalore";
 
-  numList = [12, 34, 56, 233, 565, 32];
+	numList = [12, 34, 56, 233, 565, 32];
 
-  render() {
-    return (
-      <>
-        {`My city name is ${this.city} and my mobile no is ${this.mobNo}.`}
+	render() {
+		return (
+			<>
+				{`My city name is ${this.city} and my mobile no is ${this.mobNo}.`}
 
-        {/* {this.numList.map((num) => {
+				{/* {this.numList.map((num) => {
           return <h3>{num}</h3>;
         })} */}
 
-        {this.numList.map((num) => (
-          <h3>{num}</h3>
-        ))}
-      </>
-    );
-  }
+				{this.numList.map((num) => (
+					<h3>{num}</h3>
+				))}
+			</>
+		);
+	}
+}
+
+export default Student;
+
+// 12 Oct
+
+
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class Student extends Component {
+	constructor() {
+		super();
+		console.log("This is Student Class Constructor");
+	}
+
+	ele = <h3>This is H3 Element...</h3>
+
+	mobileNumber = +7017651577;
+	city = "Bangalore";
+
+	numList = [42, 7, 83, 46, 23, 7, 6]
+
+	render() {
+		return (
+			<>
+				<h1>This is Student Component.</h1>
+
+				{this.ele}
+
+				{`My City is ` + this.mobileNumber + " . " + "My Mobile Number is " + this.city}
+
+				{this.numList.map((num) => { return <h2>{num}</h2> })}
+
+				{this.numList.map((num) => <h2>{num}</h2>)}
+			</>
+		)
+	}
 }
 
 export default Student;
--- reactclass-5/src/index.js
+++ reactclass-5/src/index.js
@@ -1,25 +1,46 @@
-import React, { createElement } from "react";
+// import React, { createElement } from "react";
+// import ReactDOM from "react-dom";
+// import Student from "./Student";
+
+// let ele1 = <h1>This Is Welcome Page !!!</h1>;
+
+// let ele2 = React.createElement("h1", null, "Welcome to Sahosoft Solutions");
+
+// let ele3 = React.createElement(
+//   "h1",
+//   null,
+//   createElement("h1", null, "Welcome to Sahosoft Solutions")
+// );
+
+// ReactDOM.render(
+//   <>
+//     {ele1}
+
+//     {ele2}
+
+//     {ele3}
+//     <Student />
+//   </>,
+//   document.getElementById("root")
+// );
+
+import React from "react";
+import { createElement } from "react";
 import ReactDOM from "react-dom";
-import Student from "./Student";
+import Student from './Student'
 
-let ele1 = <h1>This Is Welcome Page !!!</h1>;
 
-let ele2 = React.createElement("h1", null, "Welcome to Sahosoft Solutions");
+let ele = <h1>This is Welcome Page ...</h1>
 
-let ele3 = React.createElement(
-  "h1",
-  null,
-  createElement("h1", null, "Welcome to Sahosoft Solutions")
-);
+let Ele1 = createElement('div', null, 'This Is div Element')
+
+let Ele2 = createElement('div', null, createElement('h2', null, 'This H2 Nested React Create Element'))
 
 ReactDOM.render(
   <>
-    {ele1}
-
-    {ele2}
-
-    {ele3}
+    {ele}
+    {Ele1}
+    {Ele2}
     <Student />
-  </>,
-  document.getElementById("root")
-);
+  </>, document.getElementById('root')
+)
